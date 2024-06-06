1ab2403084eca0807f3014e31be2f0b71ca9fd8f
unknown <chandrua9944@gmail.com>
2024-06-06 17:33:57 +0530

    done

--- more-hooks/src/App.jsx
+++ more-hooks/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer, useRef, useState } from 'react';
+import './App.css';
+import TodoItems from './To-do-item';
 
+export const ACTION_TYPE = {
+  ADD_POST: 'add-post',
+  TOGGLE: 'toggle',
+};
+/*a*/
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
+  const inputRef = useRef();
 
+  function reducer(posts, action) {
+    switch (action.type) {
+      case ACTION_TYPE.ADD_POST:
+        return [...posts, { id: Date.now(), name: action.payload.name, toggle: true }];
+
+      case ACTION_TYPE.TOGGLE:
+        return posts.map(post =>
+          post.id === action.payload.id ? { ...post, toggle: !post.toggle } : post
+        );
+      default:
+        return posts;
+    }
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTION_TYPE.ADD_POST, payload: { name } });
+    setName('');
+  }
+  function focus() {
+    inputRef.current.focus();
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input ref={inputRef} type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </form>
+      {posts.map((post) => (
+        <TodoItems key={post.id} post={post} dispatch={dispatch} />
+      ))}
+      <button onClick={focus}>Get back writing</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
